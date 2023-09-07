8f29d3cfae15ad84112dd08b3de0019bc466e92a
jamiemetcalfe18 <jamie.jet18@gmail.com>
2023-09-07 02:14:14 +0000

    Initial commit

--- main.py
+++ main.py
@@ -34,3 +34,5 @@ while player == False:
     #player was set to True, but we want it to be False so the loop continues
     player = False
     computer = t[randint(0,2)]
+
+#yay first python project
